641f1664a34546894558c9754a7ef23c05530a17
Tacite243 <taciteiragi@gmail.com>
2024-10-01 14:57:31 +0200

    develop initial home page

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,134 +3,134 @@
 
 export default function Footer() {
     return (
-        <footer class="site-footer">
-            <div class="container">
-                <div class="row">
+        <footer className="site-footer">
+            <div className="container">
+                <div className="row">
 
-                    <div class="col-lg-12 col-12 d-flex align-items-center mb-4 pb-2">
+                    <div className="col-lg-12 col-12 d-flex align-items-center mb-4 pb-2">
                         <div>
-                            <img src="/bubbles.png" class="logo img-fluid" alt="" />
+                            <img src="/bubbles.png" className="logo img-fluid" alt="" />
                         </div>
 
-                        <ul class="footer-menu d-flex flex-wrap ms-5">
-                            <li class="footer-menu-item"><a href="#" class="footer-menu-link">A propos de nous</a></li>
+                        <ul className="footer-menu d-flex flex-wrap ms-5">
+                            <li className="footer-menu-item"><Link href="#" className="footer-menu-link">A propos de nous</Link></li>
 
-                            <li class="footer-menu-item"><a href="#" class="footer-menu-link">Contacts</a></li>
+                            <li className="footer-menu-item"><Link href="#" className="footer-menu-link">Contacts</Link></li>
                         </ul>
                     </div>
 
-                    <div class="col-lg-5 col-12 mb-4 mb-lg-0">
-                        <h5 class="site-footer-title mb-3">Nos Services</h5>
+                    <div className="col-lg-5 col-12 mb-4 mb-lg-0">
+                        <h5 className="site-footer-title mb-3">Nos Services</h5>
 
-                        <ul class="footer-menu">
-                            <li class="footer-menu-item">
-                                <a href="#" class="footer-menu-link">
-                                    <i class="bi-chevron-double-right footer-menu-link-icon me-2"></i>
+                        <ul className="footer-menu">
+                            <li className="footer-menu-item">
+                                <Link href="#" className="footer-menu-link">
+                                    <i className="bi-chevron-double-right footer-menu-link-icon me-2"></i>
                                     Lavage de vetements
-                                </a>
+                                </Link>
                             </li>
 
-                            <li class="footer-menu-item">
-                                <a href="#" class="footer-menu-link">
-                                    <i class="bi-chevron-double-right footer-menu-link-icon me-2"></i>
+                            <li className="footer-menu-item">
+                                <Link href="#" className="footer-menu-link">
+                                    <i className="bi-chevron-double-right footer-menu-link-icon me-2"></i>
                                     Netoyage des Tapis
-                                </a>
+                                </Link>
                             </li>
 
-                            <li class="footer-menu-item">
-                                <a href="#" class="footer-menu-link">
-                                    <i class="bi-chevron-double-right footer-menu-link-icon me-2"></i>
+                            <li className="footer-menu-item">
+                                <Link href="#" className="footer-menu-link">
+                                    <i className="bi-chevron-double-right footer-menu-link-icon me-2"></i>
                                     Netoyage des draps, couvertures...
-                                </a>
+                                </Link>
                             </li>
 
-                            <li class="footer-menu-item">
-                                <a href="#" class="footer-menu-link">
-                                    <i class="bi-chevron-double-right footer-menu-link-icon me-2"></i>
+                            <li className="footer-menu-item">
+                                <Link href="#" className="footer-menu-link">
+                                    <i className="bi-chevron-double-right footer-menu-link-icon me-2"></i>
                                     Repassage professionnel
-                                </a>
+                                </Link>
                             </li>
 
-                            <li class="footer-menu-item">
-                                <a href="#" class="footer-menu-link">
-                                    <i class="bi-chevron-double-right footer-menu-link-icon me-2"></i>
+                            <li className="footer-menu-item">
+                                <Link href="#" className="footer-menu-link">
+                                    <i className="bi-chevron-double-right footer-menu-link-icon me-2"></i>
                                     Services personnalisés
-                                </a>
+                                </Link>
                             </li>
                         </ul>
                     </div>
 
-                    <div class="col-lg-4 col-md-6 col-12 mb-4 mb-lg-0 mb-md-0">
-                        <h5 class="site-footer-title mb-3">Adresse physique</h5>
+                    <div className="col-lg-4 col-md-6 col-12 mb-4 mb-lg-0 mb-md-0">
+                        <h5 className="site-footer-title mb-3">Adresse physique</h5>
 
-                        <p class="text-white d-flex mt-3 mb-2">
-                            <i class="bi-geo-alt-fill me-2"></i>
+                        <p className="text-white d-flex mt-3 mb-2">
+                            <i className="bi-geo-alt-fill me-2"></i>
                             Goma 20, Av. du lac, Q.Himbi
                         </p>
 
-                        <p class="text-white d-flex mb-2">
-                            <i class="bi-telephone-fill me-2"></i>
+                        <p className="text-white d-flex mb-2">
+                            <i className="bi-telephone-fill me-2"></i>
 
-                            <a class="site-footer-link">
+                            <Link className="site-footer-link">
                                 +243 971-736-244
-                            </a>
+                            </Link>
                         </p>
 
-                        <p class="text-white d-flex">
-                            <i class="bi-envelope-fill me-2"></i>
+                        <p className="text-white d-flex">
+                            <i className="bi-envelope-fill me-2"></i>
 
-                            <a  class="site-footer-link">
+                            <Link  className="site-footer-link">
                                 info@namba.com
-                            </a>
+                            </Link>
                         </p>
 
-                        <ul class="social-icon mt-4">
-                            <li class="social-icon-item">
-                                <a href="#" class="social-icon-link button button--skoll">
+                        <ul className="social-icon mt-4">
+                            <li className="social-icon-item">
+                                <Link href="#" className="social-icon-link button button--skoll">
                                     <span></span>
-                                    <span class="bi-twitter"></span>
-                                </a>
+                                    <span className="bi-twitter"></span>
+                                </Link>
                             </li>
 
-                            <li class="social-icon-item">
-                                <a href="#" class="social-icon-link button button--skoll">
+                            <li className="social-icon-item">
+                                <Link href="#" className="social-icon-link button button--skoll">
                                     <span></span>
-                                    <span class="bi-facebook"></span>
-                                </a>
+                                    <span className="bi-facebook"></span>
+                                </Link>
                             </li>
 
-                            <li class="social-icon-item">
-                                <a href="#" class="social-icon-link button button--skoll">
+                            <li className="social-icon-item">
+                                <Link href="#" className="social-icon-link button button--skoll">
                                     <span></span>
-                                    <span class="bi-instagram"></span>
-                                </a>
+                                    <span className="bi-instagram"></span>
+                                </Link>
                             </li>
                         </ul>
                     </div>
 
-                    <div class="col-lg-3 col-md-6 col-6 mt-3 mt-lg-0 mt-md-0">
-                        <div class="featured-block">
-                            <h5 class="text-white mb-3">Horaire de Service</h5>
+                    <div className="col-lg-3 col-md-6 col-6 mt-3 mt-lg-0 mt-md-0">
+                        <div className="featured-block">
+                            <h5 className="text-white mb-3">Horaire de Service</h5>
 
-                            <strong class="d-block text-white mb-1">Lundi - Lundi</strong>
+                            <strong className="d-block text-white mb-1">Lundi - Lundi</strong>
 
-                            <p class="text-white mb-3">8h00 - 17h00</p>
+                            <p className="text-white mb-3">8h00 - 17h00</p>
                         </div>
                     </div>
                 </div>
             </div>
 
-            <div class="site-footer-bottom">
-                <div class="container">
-                    <div class="row">
+            <div className="site-footer-bottom">
+                <div className="container">
+                    <div className="row">
 
-                        <div class="col-lg-6 col-12">
-                            <p class="copyright-text mb-0">Copyright © NAMBA Co., Ltd.</p>
+                        <div className="col-lg-6 col-12">
+                            <p className="copyright-text mb-0">Copyright © NAMBA Co., Ltd.</p>
                         </div>
 
-                        <div class="col-lg-6 col-12 text-end">
-                            <p class="copyright-text mb-0">
-                            Designed by <a href="https://www.tooplate.com" target="_parent">NAMBA IT TEAM</a></p>
+                        <div className="col-lg-6 col-12 text-end">
+                            <p className="copyright-text mb-0">
+                            Designed by <Link href="https://www.tooplate.com" target="_parent">NAMBA IT TEAM</Link></p>
                         </div>
 
                     </div>
--- src/components/serviceCard.jsx
+++ src/components/serviceCard.jsx
@@ -4,24 +4,24 @@ import { Link } from "react-router-dom";
 
 export default function ServiceCard({data}) {
     return (
-        <div class="col-lg-6 col-12">
-            <div class="services-thumb">
-                <div class="row">
-                    <div class="col-lg-5 col-md-5 col-12">
-                        <div class="services-image-wrap">
+        <div className="col-lg-6 col-12">
+            <div className="services-thumb">
+                <div className="row">
+                    <div className="col-lg-5 col-md-5 col-12">
+                        <div className="services-image-wrap">
                             <Link href="services-detail.html">
-                                <img src={data.avatar} class="services-image img-fluid" alt="" />
-                                <img src={data.hover} class="services-image services-image-hover img-fluid" alt="" />
+                                <img src={data.avatar} className="services-image img-fluid" alt="" />
+                                <img src={data.hover} className="services-image services-image-hover img-fluid" alt="" />
 
-                                <div class="services-icon-wrap">
-                                    <div class="d-flex justify-content-between align-items-center">
-                                        <p class="text-white mb-0">
-                                            <i class="bi-cash me-2"></i>
+                                <div className="services-icon-wrap">
+                                    <div className="d-flex justify-content-between align-items-center">
+                                        <p className="text-white mb-0">
+                                            <i className="bi-cash me-2"></i>
                                             ${data.tarif.amount}
                                         </p>
 
-                                        <p class="text-white mb-0">
-                                            <i class="bi-clock-fill me-2"></i>
+                                        <p className="text-white mb-0">
+                                            <i className="bi-clock-fill me-2"></i>
                                             {data.tarif.unity}
                                         </p>
                                     </div>
@@ -30,34 +30,34 @@ export default function ServiceCard({data}) {
                         </div>
                     </div>
 
-                    <div class="col-lg-7 col-md-7 col-12 d-flex align-items-center">
-                        <div class="services-info mt-4 mt-lg-0 mt-md-0">
-                            <h4 class="services-title mb-1 mb-lg-2">
-                                <a class="services-title-link" href="services-detail.html">{data.title}</a>
+                    <div className="col-lg-7 col-md-7 col-12 d-flex align-items-center">
+                        <div className="services-info mt-4 mt-lg-0 mt-md-0">
+                            <h4 className="services-title mb-1 mb-lg-2">
+                                <Link className="services-title-link" href="services-detail.html">{data.title}</Link>
                             </h4>
 
                             <p>{data.description}</p>
 
-                            <div class="d-flex flex-wrap align-items-center">
-                                <div class="reviews-icons">
-                                    <i class="bi-star-fill"></i>
-                                    <i class="bi-star-fill"></i>
-                                    <i class="bi-star-fill"></i>
-                                    <i class="bi-star"></i>
-                                    <i class="bi-star"></i>
+                            <div className="d-flex flex-wrap align-items-center">
+                                <div className="reviews-icons">
+                                    <i className="bi-star-fill"></i>
+                                    <i className="bi-star-fill"></i>
+                                    <i className="bi-star-fill"></i>
+                                    <i className="bi-star"></i>
+                                    <i className="bi-star"></i>
                                 </div>
 
-                                <a href="services-detail.html" class="custom-btn btn button button--atlas mt-2 ms-auto">
+                                <Link href="services-detail.html" className="custom-btn btn button button--atlas mt-2 ms-auto">
                                     <span>Réserver</span>
 
-                                    <div class="marquee" aria-hidden="true">
-                                        <div class="marquee__inner">
+                                    <div className="marquee" aria-hidden="true">
+                                        <div className="marquee__inner">
                                             <span>Pas la peine</span>
                                             <span>Pas la peine de vous deplacer</span>
                                             <span>deplacer</span>
                                         </div>
                                     </div>
-                                </a>
+                                </Link>
                             </div>
                         </div>
                     </div>
