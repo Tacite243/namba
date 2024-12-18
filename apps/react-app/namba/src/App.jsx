66726f9dc9a34253be562e1d1ae4b30255132ca1
Tacite243 <taciteiragi@gmail.com>
2024-12-18 21:58:05 +0200

    feat: create collector home page

--- apps/react-app/namba/src/App.jsx
+++ apps/react-app/namba/src/App.jsx
@@ -5,6 +5,7 @@ import './styles/bootstrap-icons.css';
 import './styles/bootstrap.min.css';
 import './styles/magnific-popup.css';
 import './styles/tooplate-clean-work.css'
+// import './App.css';
 import AppRoutes from './routes/AppRoutes';
 import Header from './components/header';
 import NavBar from './components/navBar';
