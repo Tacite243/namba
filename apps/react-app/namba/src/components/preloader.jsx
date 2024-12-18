eff7ff71e426a77c5b698b2bb3fa8e2c458650b6
Tacite243 <taciteiragi@gmail.com>
2024-12-19 01:37:44 +0200

    feat: add preloader

---
+++ apps/react-app/namba/src/components/preloader.jsx
@@ -0,0 +1,15 @@
+
+
+const Preloader = () => {
+    return (
+        <div className="preloader">
+            <div className="loader">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </div>
+    );
+};
+
+export default Preloader;
