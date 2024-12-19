4daf437181ff793856f30c7f37224e11aa975520
Tacite243 <taciteiragi@gmail.com>
2024-12-19 21:49:09 +0200

    feat: create admin dashboard page

--- apps/react-app/namba/src/App.jsx
+++ apps/react-app/namba/src/App.jsx
@@ -25,8 +25,8 @@ export default function App() {
   return (
     <>
       {loading ? <Preloader /> : <>
-        {/* <Header />
-        <NavBar /> */}
+        <Header />
+        <NavBar />
         <AppRoutes />
         <Footer />
       </>}
